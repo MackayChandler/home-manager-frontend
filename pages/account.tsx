265026a83ff14d5a0d6147b0c95ab039030b34a9
MackayChandler <55098733+MackayChandler@users.noreply.github.com>
2023-07-03 16:55:56 -0600

    feat: added some fields to account page

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,10 +1,27 @@
-import { Container, Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Container,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import * as React from "react";
 
 export default function Account() {
   return (
     <Container>
-      <Typography>This is the Account Page</Typography>
+      <Card>
+        <CardHeader title="Account Information" />
+        <CardContent>
+          <Stack>
+            <TextField label="First Name" margin="normal" />
+            <TextField label="Last Name" margin="normal" />
+            <TextField label="Email" margin="normal" />
+          </Stack>
+        </CardContent>
+      </Card>
     </Container>
   );
 }
